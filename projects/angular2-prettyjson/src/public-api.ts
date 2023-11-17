51880d1e7f6e74b7a9f41d36afa1534c34375083
Leonardo Cavone <lcavone@talentia-software.com>
2023-11-17 18:17:13 +0100

    angular v15.2.10

---
+++ projects/angular2-prettyjson/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of @talentia/angular2-prettyjson
+ */
+
+export { SafeJsonPipe } from './lib/json.pipe';
+export { PrettyJsonPipe } from './lib/prettyjson.pipe';
+export { PrettyJsonComponent } from './lib/prettyjson.component';
+export { PrettyJsonModule } from './lib/prettyjson.module';
